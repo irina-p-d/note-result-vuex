105ecde7cf41bf7bdf16f03c65cdad93b68f8a9a
irina-p-d <irinapetrovap@gmail.com>
2022-02-15 10:05:15 +0100

    structur of project created

---
+++ src/components/Notes.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+            <h3>Introduce tu nota</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Notes"
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Para obtener la información sobre tu cualificación pulsa el botón "Obtener cualificación"</p>
+ <button>Obtener cualificación</button>
+   
+  </div>  
+      
+</template>
+
+<script>
+export default {
+  name: 'Bienvenida',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+p {
+  font-size: 18px;
+  margin: 3% auto;
+}
+button {
+  display: block;
+  margin: 3% auto;
+  padding: 1.5%;
+  background-color: #42b983;
+  font-weight: bold;
+  color: #35495E;
+  border-radius: 5px;
+}
+</style>
